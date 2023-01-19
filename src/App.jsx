22f0d0aaba069676119ce6663507f9d4420c4b56
roob1y <robwork122@gmail.com>
2023-01-19 15:39:54 +0000

    Creates cards to display area data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState, useCallback } from "react";
 import { getAreaData } from "./api";
+import OutcodeCard from "./components/OutcodeCard";
 
 import "./App.css";
 
 function App() {
   const [areas, setAreas] = useState([]);
   const [outcode, setOutcode] = useState("BB10");
-  const [outcodeDisplay, setOutcodeDisplay] = useState("BB10")
+  const [outcodeDisplay, setOutcodeDisplay] = useState("BB10");
 
   const load = async () => {
     try {
@@ -24,11 +25,14 @@ function App() {
     load();
   }, []);
 
-  const handleSubmit = useCallback((event) => {
-    event.preventDefault();
-    setOutcodeDisplay(outcode);
-    load()
-  }, [outcode]);
+  const handleSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+      setOutcodeDisplay(outcode);
+      load();
+    },
+    [outcode]
+  );
 
   return (
     <div className="App">
@@ -36,11 +40,21 @@ function App() {
       <form onSubmit={handleSubmit}>
         <label htmlFor="outcode">Outcode</label>
         <br />
-        <textarea name="outcode" id="outcode" onChange={(e) => setOutcode(e.target.value)}></textarea> <br />
+        <textarea
+          name="outcode"
+          id="outcode"
+          onChange={(e) => setOutcode(e.target.value)}
+        ></textarea>{" "}
+        <br />
         <br />
         <input type="submit" value="Submit" aria-label="submit outcode" />
       </form>
       <h2>{`Areas for ${outcodeDisplay}: ${areas.length}`}</h2>
+      <div style={{display: "flex", flexWrap: "wrap", gap: "10px"}}>
+        {areas.map((area) => {
+          return <OutcodeCard area={area} key={area["place name"]} />;
+        })}
+      </div>
     </div>
   );
 }
