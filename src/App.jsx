ea294220ec57e4e6a21cb788ed05fbfa8098de4b
roob1y <robwork122@gmail.com>
2023-01-19 15:04:14 +0000

    task 3 outcode user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
-import { useEffect, useState } from 'react'
-import { getAreaData } from './api'
+import { useEffect, useState, useCallback } from "react";
+import { getAreaData } from "./api";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
   const [areas, setAreas] = useState([]);
+  const [outcode, setOutcode] = useState("BB10");
+  const [outcodeDisplay, setOutcodeDisplay] = useState("BB10")
 
   const load = async () => {
     try {
-      const areaData = await getAreaData()
+      const areaData = await getAreaData(outcode);
 
       areas.concat(areaData);
-  
+
       setAreas(areaData);
     } catch (error) {
-      window.alert("todo: fix app")
+      window.alert("todo: fix app");
     }
-  }
+  };
 
   useEffect(() => {
     load();
   }, []);
 
+  const handleSubmit = useCallback((event) => {
+    event.preventDefault();
+    setOutcodeDisplay(outcode);
+    load()
+  }, [outcode]);
+
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="outcode">Outcode</label>
+        <br />
+        <textarea name="outcode" id="outcode" onChange={(e) => setOutcode(e.target.value)}></textarea> <br />
+        <br />
+        <input type="submit" value="Submit" aria-label="submit outcode" />
+      </form>
+      <h2>{`Areas for ${outcodeDisplay}: ${areas.length}`}</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
